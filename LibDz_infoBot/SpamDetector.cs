277308359904469168fed04645a07970c5e0216d
Lib_int <130091860+Liberi@users.noreply.github.com>
2023-12-07 18:12:38 +0300

    Add files via upload

---
+++ LibDz_infoBot/SpamDetector.cs
@@ -0,0 +1,39 @@
+﻿
+namespace LibDz_infoBot
+{
+    public class SpamDetector
+    {
+        private Dictionary<long, List<DateTime>> userMessages;
+
+        public SpamDetector()
+        {
+            userMessages = new Dictionary<long, List<DateTime>>();
+        }
+
+        public bool IsSpam(long userId)
+        {
+            const int maxMessages = 5;
+            TimeSpan timeWindow = TimeSpan.FromSeconds(3);
+
+            if (!userMessages.ContainsKey(userId))
+            {
+                userMessages.Add(userId, new List<DateTime>());
+            }
+
+            var messages = userMessages[userId];
+            var currentTime = DateTime.Now;
+
+            messages.RemoveAll(msg => currentTime - msg > timeWindow);
+            messages.Add(currentTime);
+
+            if (messages.Count > maxMessages)
+            {
+                var oldestMessageTime = messages[0];
+                var timeElapsed = currentTime - oldestMessageTime;
+                return timeElapsed < timeWindow;
+            }
+
+            return false;
+        }
+    }
+}
